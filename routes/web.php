5a6c3320101b14a7c45be7a4855876966521ddff
keno12345678 <mastercole12345@gmail.com>
2024-05-28 11:41:59 -0500

    empresa 1.2

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route :: get('/login', function(){
+    return view('auth.login');
+});
+    Route :: get('/logout', function(){
+    return redirect('/');
+}); 
+    Route :: get('/catalog', function(){
+    return view ('catalog.index');
+}); 
+    Route :: get('/catalog/show/{id}', function($id){
+    return view ('catalog.show',['id'=>$id]);
+}); 
+    Route :: get('/catalog/create', function(){
+    return view ('catalog.create');
+}); 
+    Route :: get('/catalog/edit/{id}', function($id){
+    return view ('catalog.edit',['id'=>$id]);
 });
